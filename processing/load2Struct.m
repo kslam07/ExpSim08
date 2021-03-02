cb471d16a1c26cdf2ea59cb6f5af4aae642c36c4
kslam <trykslam@gmail.com>
2021-03-02 09:54:58 +0100

    Add the coefficient columns back in because apparently we need them

--- processing/load2Struct.m
+++ processing/load2Struct.m
@@ -9,11 +9,6 @@ function dataStruct = load2Struct(outputPath)
     dataBeta10 = readtable(outputPath, "sheet", "dir_stab_rudder_ten");
     dataBeta5 = readtable(outputPath, "sheet", "dir_stab_rudder_five");
     
-    dataPropoff = dataPropoff(:, 1:35);
-    dataBeta0 = dataBeta0(:, 1:35);
-    dataBeta5 = dataBeta5(:, 1:35);
-    dataBeta10 = dataBeta10(:, 1:35);
-    
     dataStruct = struct("propoff", dataPropoff, "beta0", dataBeta0, ... 
                         "beta5", dataBeta5, "beta10", dataBeta10);
 
