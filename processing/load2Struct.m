8f6072569cd587aa439d0c707220c6ef9907acd3
kslam07 <trykslam@gmail.com>
2021-03-16 22:44:16 +0100

    Change delta value for streamline curvature correction (upwash)
    Changed delta which at first was determined using just the span.
    However, the effective span must be used using figures from
    Barlow et. al.

--- processing/load2Struct.m
+++ processing/load2Struct.m
@@ -21,34 +21,35 @@ function dataStruct = load2Struct(measPath, tailOffPath)
     XmRefM      = [0.25,0,0];       % moment ref. in model reference system
 
     % wind tunnel dimensions
-    tunnelWidth = 1.8;          % [m]
-    tunnelHeight = 1.25;        % [m]
-    tunnelFillet = 0.3;         % [m]
-    tunnelArea = tunnelWidth*tunnelHeight-4*(0.5*tunnelFillet^2);
+    tunnelWidth = 1.8;                                  % [m]
+    tunnelHeight= 1.25;                                 % [m]
+    tunnelFillet= 0.3;                                  % [m]
+    tunnelArea  = tunnelWidth*tunnelHeight-4*(0.5*tunnelFillet^2);
     
     % streamline curvature correction factors
-    % tail length taken to be c/2
-    delta = 0.106;                                  % interference factor
-    tau2 = 0.122;                                   % downwash corr. factor
+    % tail length taken to be c/2; b_eff = 1.21582
+    delta       = 0.1033;                                % interference
+    tau2        = 0.1458;                                % upwash corr.
     
     % blockage factors
-    eps_sb = 0.005394188570670;                     % solid blockage 
-    eps_wb = 0;                                     % [WiP] change this
+    epsSB       = 0.005394188570670;                    % solid blockage 
+    epsWB       = 0;                                    % [WiP] change this
+    eps         = epsSB + epsWB;                        % total blockage
     
-    dataPropoff = readtable(measPath, "sheet", ... 
-        "beta_sweep_alfa_2_propoff");
+    dataPropoff = readtable(measPath,"sheet","beta_sweep_alfa_2_propoff");
     dataTailOffAoA = readtable(tailOffPath, "sheet", "AoS = 0 deg");
     dataTailOffAoS = readtable(tailOffPath, "sheet", "AoS variations");
     
-    dataBeta0 = readtable(measPath, "sheet", "dir_stab_rudder_zero");
-    dataBeta10 = readtable(measPath, "sheet", "dir_stab_rudder_ten");
-    dataBeta5 = readtable(measPath, "sheet", "dir_stab_rudder_five");
+    dataRud0 = readtable(measPath, "sheet", "dir_stab_rudder_zero");
+    dataRud10 = readtable(measPath, "sheet", "dir_stab_rudder_ten");
+    dataRud5 = readtable(measPath, "sheet", "dir_stab_rudder_five");
     
-    dataStruct = struct("i0", struct("propoff", dataPropoff, "beta0", ... 
-                 dataBeta0, "beta5", dataBeta5, "beta10", dataBeta10), ...
+    dataStruct = struct("i0", struct("propoff", dataPropoff, "rud0", ... 
+                 dataRud0, "rud5", dataRud5, "rud10", dataRud10), ...
                  "sRef", S, "span", b, "cRef", c, "Dprop", D, ...
                  "locRefB", XmRefB, "locRefM", XmRefM, "delta", delta, ...
                  "tau2", tau2, "tailoffAoA", dataTailOffAoA, ...
                  "tailoffAoS", dataTailOffAoS, "tunnelArea", tunnelArea,...
-                 "tunnelWidth", tunnelWidth, "tunnelHeight", tunnelHeight);
+                 "tunnelWidth", tunnelWidth, "tunnelHeight", ... 
+                 tunnelHeight, "epsSB", epsSB, "epsWB", epsWB, "eps", eps);
 end
