7ad55b047083a25a03c5ae410e01b98e57fd3d64
ltyap <litoong.yap@gmail.com>
2021-03-17 12:58:40 +0100

    minor update

--- processing/corrWakeblockage.m
+++ processing/corrWakeblockage.m
@@ -8,14 +8,14 @@ function eps_wb = corrWakeblockage(dataStruct, WingArea, WTCrossSection)
     
     for iName=1:length(fieldNames)
         data = dataTable.(cell2mat(fieldNames(iName)));
-        Cl2 = (data.Cl).^2;
+        Cl2 = (data.CL).^2;
         polar = sort([Cl2 data.Cd]);
         
         p = polyfit(polar(1:3,1),polar(1:3,2) , 1);
         Cdi = p(1);
         
         
-        Cd0 = min(data.Cd);
+        Cd0 = min(data.CD);
         tmp = Cd - Cdi - Cd0;%vector?
         tmp(tmp<0) = 0;
     
