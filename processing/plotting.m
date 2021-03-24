6f03e8582efe5a8b2a53ececf035ce29d672a7e4
ThomasRV <thomas.verduyn@gmail.com>
2021-03-25 00:25:57 +0100

    update plotjes

--- processing/plotting.m
+++ processing/plotting.m
@@ -33,6 +33,115 @@ function [] = plotting(data)
     rud1040=sortrows(data.i0.rud10(idx1040,:),'AoS');
     rud1040b=sortrows(data.i0.rud10(idx1040b,:),'AoS');
     
+    %% loop
+    err=0.05;
+    
+    % col: 0, 5, 10 delta_r 
+    % row: v=20 & 1 engine | v=20 & 2 engine | v=40 & 1 engine | ...
+%     zeros(N, 3);
+    CYArr20 = [];
+    CYArr20b = [];
+    CYArr40 = [];
+    CYArr40b = [];
+    
+    AOS = [-10 -6:2:6 10];
+    for iAOS = 1:length(AOS)
+        
+        % get the data
+        CY020 = rud020(abs(AOS(iAOS)-rud020.AoS)<err, :);
+        CY520 = rud520(abs(AOS(iAOS)-rud520.AoS)<err, :);
+        CY1020 = rud1020(abs(AOS(iAOS)-rud1020.AoS)<err, :);
+        CYArr20(iAOS,:) = [CY020.CY CY520.CY CY1020.CY];
+        
+        if AOS(iAOS)==-2
+            CY020b = rud020b(4, :);
+            CY520b = rud520b(abs(AOS(iAOS)-rud520b.AoS)<err, :);
+            CY1020b = rud1020b(abs(AOS(iAOS)-rud1020b.AoS)<err, :);
+            CYArr20b(iAOS,:) = [CY020b.CY CY520b.CY CY1020b.CY];
+            
+        elseif AOS(iAOS)==0
+            CYArr20b(iAOS,:) = [0 0 0];
+        else
+            CY020b = rud020b(abs(AOS(iAOS)-rud020b.AoS)<err, :);
+            CY520b = rud520b(abs(AOS(iAOS)-rud520b.AoS)<err, :);
+            CY1020b = rud1020b(abs(AOS(iAOS)-rud1020b.AoS)<err, :);
+            CYArr20b(iAOS,:) = [CY020b.CY CY520b.CY CY1020b.CY];
+        end
+        
+        if AOS(iAOS)==-10
+            CY040 = rud040(1, :);
+            CY540 = rud540(abs(AOS(iAOS)-rud540.AoS)<err, :);
+            CY1040 = rud1040(abs(AOS(iAOS)-rud1040.AoS)<err, :);
+            CYArr40(iAOS,:) = [CY040.CY CY540.CY CY1040.CY];
+        elseif AOS(iAOS)==-4
+            CY040b = rud040(abs(AOS(iAOS)-rud040.AoS)<err, :);
+            CY540b = rud540(abs(AOS(iAOS)-rud540.AoS)<err, :);
+            CY1040b = rud1040(3, :);
+            CYArr40b(iAOS,:) = [CY040b.CY CY540b.CY CY1040b.CY];
+        else
+            CY040 = rud040(abs(AOS(iAOS)-rud040.AoS)<err, :);
+            CY540 = rud540(abs(AOS(iAOS)-rud540.AoS)<err, :);
+            CY1040 = rud1040(abs(AOS(iAOS)-rud1040.AoS)<err, :);
+            CYArr40(iAOS,:) = [CY040.CY CY540.CY CY1040.CY];
+        end
+        
+
+            CY040b = rud040(abs(AOS(iAOS)-rud040b.AoS)<err, :);
+            CY540b = rud540(abs(AOS(iAOS)-rud540b.AoS)<err, :);
+            CY1040b = rud1040(abs(AOS(iAOS)-rud1040b.AoS)<err, :);
+            CYArr40b(iAOS,:) = [CY040b.CY CY540b.CY CY1040b.CY];
+        
+    end
+    
+    dCYddelta20 = [];
+    dCYddelta20b = [];
+    dCYddelta40 = [];
+    dCYddelta40b = [];
+    
+    for idx=1:length(CYArr20)
+        fit=polyfit([0 5 10], CYArr20(idx,:),1);
+        dCYddelta20(idx)=fit(1);
+    end
+    for idx=1:length(CYArr20b)
+        fit=polyfit([0 5 10], CYArr20b(idx,:),1);
+        dCYddelta20b(idx)=fit(1);
+    end    
+    for idx=1:length(CYArr40)
+        fit=polyfit([0 5 10], CYArr40(idx,:),1);
+        dCYddelta40(idx)=fit(1);
+    end
+    for idx=1:length(CYArr40b)
+        fit=polyfit([0 5 10], CYArr40b(idx,:),1);
+        dCYddelta40b(idx)=fit(1);
+    end       
+    
+    
+    dcydd20=mean(dCYddelta20);
+    dcydd20b=mean(dCYddelta20b);
+    dcydd40=mean(dCYddelta40);
+    dcydd40b=mean(dCYddelta40b);
+    
+    dcydb20=polyfit(rud020.AoS,rud020.CY,1);
+    dcydb20b=polyfit(rud020b.AoS,rud020b.CY,1);
+    dcydb40=polyfit(rud040.AoS,rud040.CY,1);
+    dcydb40b=polyfit(rud040b.AoS,rud040b.CY,1);
+    
+    
+%     subplot(2,2,1)
+%     scatter([-10 -6:2:6 10],dCYddelta20)
+%     hold on
+%     yline(dcydd20)
+%     subplot(2,2,2)
+%     scatter([-10 -6:2:6 10],dCYddelta20b)
+%     yline(dcydd20b)
+%     subplot(2,2,3)
+%     scatter([-10 -6:2:6 10],dCYddelta40)
+%     yline(dcydd40)
+%     subplot(2,2,4)
+%     scatter([-10 -6:2:6 10],dCYddelta40b)
+%     yline(dcydd40b)
+    
+    
     %% Plotting
     figure(1)
     t = tiledlayout(2,2,'TileSpacing','Compact','Padding','Compact');
@@ -47,14 +156,14 @@ function [] = plotting(data)
     ylabel('C_Y')
     grid on
     nexttile
-    plot(rud020.AoS,rud020.CN,'x')
+    plot(rud020.AoS,rud020.CD,'x')
     hold on
-    plot(rud020.AoS,polyval(polyfit(rud020.AoS,rud020.CN,3),rud020.AoS))
-    plot(rud020b.AoS,rud020b.CN,'o')
-    plot(rud020b.AoS,polyval(polyfit(rud020b.AoS,rud020b.CN,3),rud020b.AoS))
+    plot(rud020.AoS,polyval(polyfit(rud020.AoS,rud020.CD,3),rud020.AoS))
+    plot(rud020b.AoS,rud020b.CD,'o')
+    plot(rud020b.AoS,polyval(polyfit(rud020b.AoS,rud020b.CD,3),rud020b.AoS))
     xlim([-10,10])
     xlabel('\beta')
-    ylabel('C_N')
+    ylabel('C_D')
     grid on
     nexttile
     plot(rud020.AoS,rud020.CMr,'x')
@@ -90,14 +199,14 @@ function [] = plotting(data)
     ylabel('C_Y')
     grid on
     nexttile
-    plot(rud040.AoS,rud040.CN,'x')
+    plot(rud040.AoS,rud040.CD,'x')
     hold on
-    plot(rud040.AoS,polyval(polyfit(rud040.AoS,rud040.CN,3),rud040.AoS))
-    plot(rud040b.AoS,rud040b.CN,'o')
-    plot(rud040b.AoS,polyval(polyfit(rud040b.AoS,rud040b.CN,3),rud040b.AoS))
+    plot(rud040.AoS,polyval(polyfit(rud040.AoS,rud040.CD,3),rud040.AoS))
+    plot(rud040b.AoS,rud040b.CD,'o')
+    plot(rud040b.AoS,polyval(polyfit(rud040b.AoS,rud040b.CD,3),rud040b.AoS))
     xlim([-10,10])
     xlabel('\beta')
-    ylabel('C_N')
+    ylabel('C_D')
     grid on
     nexttile
     plot(rud040.AoS,rud040.CMr,'x')
